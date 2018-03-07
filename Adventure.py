be32e86427bc4700b24a7eb2139074f899b34ec1
jahalome04 <Jahalome04@e3civichigh.com>
2018-03-07 15:10:01 -0800

    I added in the grue, inventory, and a flashlight that can turn on. I am also working on making a grue appear in a room based on percentage but I'm finding how to code that.

--- Adventure.py
+++ Adventure.py
@@ -1,12 +1,12 @@
 from room import Room
-from Inventory import Inventory
+from Inventory import Inventory, Flashlight, DarkRoom
 
 helipad = Room('You have parachuted from a helicopter on a special mission to save the president inside of the Empire State Building from ZOMBIES', 'You are on the Empire State Building helipad and start to look around for a way to enter the building', 'h')
 staircase = Room('You open the door that leads to the staircase and you proceed to go down','After fending off 15 zombies you are now inside the building', 'd')
 hallway = Room('You start to walk down the hallway until you are ambushed by a herd of zombies and you pull out your pistol and shoot at them in an attempt to fight them off', 'After facing off with an enormous herd of zombies you are now exhausted but ready to continue on with your mission', 'h')
 lobby = Room('In order to avoid another encounter with zombies you crouch down and start to apply your cloaking device to avoid detection. You start to carefully deploy explosives in various areas of the lobby. After deploying the explosives, you walk back a safe distance from the explosives and proceed to detonate them. ', 'Now that you have successfully eliminated all the zombies in the you stand in the middle of the blood ridden lobby, thinking about all the choices you made that led you to this point in life', 'uh')
 kitchen = Room('After contemplating life you look around for another way to get deeper into the building and you spot a door that leads into the kitchen', 'After many attempts at forcefully pushing the door open, you are finally inside the kitchen and there still seems to be no sign of the president', 'b1')
-ceo_office = Room('You continue to venture deeper into the kitchen and you stumble upon the CEO office', 'The door of the office was wide open so you walked in but as soon as you entered the office you were attacked by a zombie that came out from behind the office door but fortunately the zombie had no legs so you stomped it out', 'b2')
+ceo_office = DarkRoom('You continue to venture deeper into the kitchen and you stumble upon the CEO office', 'The door of the office was wide open so you walked in but as soon as you entered the office you were attacked by a zombie that came out from behind the office door but fortunately the zombie had no legs so you stomped it out', 'b2')
 bedroom3 = Room('bedroom', 'You are in a bedroom', 'b3')
 living = Room('living Room', 'You are in the living room', 'lr')
 
@@ -21,6 +21,8 @@ current_room.enter_room()
 
 inventory = Inventory()
 current_room = helipad
+
+kitchen.add_item(Flashlight())
 
 while True:
     current_room.enter_room( )
--- Inventory.py
+++ Inventory.py
@@ -9,48 +9,160 @@ class Inventory():
         self.items.remove(item)
 
     def list(self):
-        print "You are carrying:"
+        print ("You are carrying:")
         for item in self.items:
-            print item.get_name()
+            print (item.get_name())
+
+    def get(self, type):
+        items_of_type = []
+        for item in self.items:
+            if isinstance(item, type):
+                items_of_type.append(item)
+        return items_of_type
+
+    def process_command(self, command):
+        result = []
+        for item in self.items:
+            if item.get_name() in command:
+                result.append(item.process_command(command))
+        return result
+
 
 class Item():
     def __init__(self, name):
         self.name = name
+        self.known_commands = {}
 
     def get_name(self):
         return self.name
 
+    def process_command(self, command):
+        for a_command in self.known_commands:
+            if a_command in command:
+                self.known_commands[a_command](command)
 
 
 class Literature(Item):
-    def __init__(self,name):
-        Item.__init__(self, name, contents ="This item is blank.")
+    def __init__(self, name, contents="This item is blank."):
+        Item.__init__(self, name)
+        self.contents = contents
+        self.known_commands["read"] = self.read
+        self.known_commands["write"] = self.write(command)
+
+    def read(self, command):
+        print (self.contents)
+
+    def write(self, contents):
         self.contents = contents
 
-    def read(self):
-        print self.contents
 
-    def set_contents(self, contents):
-        self.contents = contents
+class Room():
+    def __init__(self, name, description, id):
+        self.name = name
+        self.description = description
+        self.id = id
+        self.items = []
+        self.connectors = []
+        self.rooms = {}
 
-class Flashlight(Item):
-    def __init__(self, name, battery_level=100):
-        Item. __init__(self, name)
+    def add_item(self, item):
+        self.items.append(item)
+
+    def add_room(self, direction, room):
+        self.rooms[direction] = room
+
+    def add_connection(self, room, connector, actions):
+        for direction in actions:
+            self.rooms[direction] = room
+        self.connectors.append((connector, actions[0]))
+
+    def enter_room(self, inventory):
+        print (self.name)
+        print
+        print (self.description)
+        print
+        for connector in self.connectors:
+            print ("There is a " + connector[0] + \
+                   " that goes " + connector[1] + ".")
+        print
+        for item in self.items:
+            print ("You see a " + item.name + " here.")
+        print
+
+    def get_name(self):
+        return self.name
+
+    def is_valid_direction(self, direction):
+        return direction in self.rooms.keys()
+
+    def next_room(self, direction):
+        return self.rooms[direction]
+
+    def process_command(self, command, inventory):
+        if command in self.rooms.keys():
+            new_room = self.next_room(command)
+            return new_room
+        elif "get" in command:
+            for item in self.items:
+                if item.name in command:
+                    inventory.add(item)
+                    self.items.remove(item)
+                    return "You picked up the " + item.name + "."
+                else:
+                    return "I don't know what you want to pick up."
+        else:
+            return None
+
+
+class LightSource(Item):
+    def __init__(self, name, on=False):
+        self.on = on
+        Item.__init__(self, name)
+        self.known_commands["turn on"] = self.turn_on
+        self.known_commands["turn off"] = self.turn_off
+
+    @staticmethod
+    def is_one_on(sources):
+        if len(sources) > 0:
+            for source in sources:
+                if source.is_on():
+                    return True
+        return False
+
+    def is_on(self):
+        return self.on
+
+    def turn_on(self, command):
+        self.on = True
+        print ("The " + self.name + " is on.")
+
+    def turn_off(self, command):
+        self.on = False
+        print ("The " + self.name + " is off.")
+
+
+class Flashlight(LightSource):
+    def __init__(self, name="flashlight", battery_level=100, on=False):
+        LightSource.__init__(self, name, on)
         self.battery_level = battery_level
-        self.state = state
-
-    def turn_on(self):
-        self.state = "On"
-
-    def turn_off(self):
-        self.state = "Off"
 
     def change_batteries(self):
-        self.battery_level=100
+        self.battery_level = 100
 
     def compute_usage(self):
+        # Compute the time it's been on and then drain the battery an equal amount
         pass
 
+
+class DarkRoom(Room):
+    def enter_room(self, inventory):
+        light_sources = inventory.get(LightSource)
+        if LightSource.is_one_on(light_sources):
+            Room.enter_room(self, inventory)
+        else:
+            print ("You were eaten by a grue.")
+            print ("Game over.")
+            exit()
 
 
 
